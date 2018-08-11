499474c755c5d0da0e3eee71378bcbe9656ca15f
chinalee23 <chinalee_lzh@126.com>
2018-08-11 23:38:45 +0800

    镜头脚本

---
+++ MathUtils.cs
@@ -0,0 +1,548 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// 数学方法集合
+/// </summary>
+static public class MathUtils
+{
+    static public Quaternion LookDir(Vector3 kDir, bool ignoreY = true)
+    {
+        if (ignoreY)
+            kDir.y = .0f;
+
+        kDir.Normalize();
+
+        Quaternion rot = Quaternion.identity;
+
+        if (kDir.magnitude > 0.01)
+        {
+            rot.SetLookRotation(kDir);
+        }
+        return rot;
+    }
+
+    #region Easing Curves
+    //curveURL： http://robertpenner.com/easing/easing_demo.html
+
+    public enum EaseType
+    {
+        easeInQuad,
+        easeOutQuad,
+        easeInOutQuad,
+        easeInCubic,
+        easeOutCubic,
+        easeInOutCubic,
+        easeInQuart,
+        easeOutQuart,
+        easeInOutQuart,
+        easeInQuint,
+        easeOutQuint,
+        easeInOutQuint,
+        easeInSine,
+        easeOutSine,
+        easeInOutSine,
+        easeInExpo,
+        easeOutExpo,
+        easeInOutExpo,
+        easeInCirc,
+        easeOutCirc,
+        easeInOutCirc,
+        linear,
+        spring,
+        /* GFX47 MOD START */
+        //bounce,
+        easeInBounce,
+        easeOutBounce,
+        easeInOutBounce,
+        /* GFX47 MOD END */
+        easeInBack,
+        easeOutBack,
+        easeInOutBack,
+        /* GFX47 MOD START */
+        //elastic,
+        easeInElastic,
+        easeOutElastic,
+        easeInOutElastic,
+        /* GFX47 MOD END */
+        punch
+    }
+
+    public static float ease(float start, float end, float value, EaseType easeType)
+    {
+        float result = .0f;
+        switch(easeType)
+        {
+            case EaseType.easeInQuad:
+                result = easeInQuad(start, end, value);
+                break;
+            case EaseType.easeOutQuad:
+                result = easeOutQuad(start, end, value);
+                break;
+            case EaseType.easeInOutQuad:
+                result = easeInOutQuad(start, end, value);
+                break;
+            case EaseType.easeInCubic:
+                result = easeInCubic(start, end, value);
+                break;
+            case EaseType.easeOutCubic:
+                result = easeOutCubic(start, end, value);
+                break;
+            case EaseType.easeInOutCubic:
+                result = easeInOutCubic(start, end, value);
+                break;
+            case EaseType.easeInQuart:
+                result = easeInQuart(start, end, value);
+                break;
+            case EaseType.easeOutQuart:
+                result = easeOutQuart(start, end, value);
+                break;
+            case EaseType.easeInOutQuart:
+                result = easeInOutQuart(start, end, value);
+                break;
+            case EaseType.easeInQuint:
+                result = easeInQuint(start, end, value);
+                break;
+            case EaseType.easeOutQuint:
+                result = easeOutQuint(start, end, value);
+                break;
+            case EaseType.easeInOutQuint:
+                result = easeInOutQuint(start, end, value);
+                break;
+            case EaseType.easeInSine:
+                result = easeInSine(start, end, value);
+                break;
+            case EaseType.easeOutSine:
+                result = easeOutSine(start, end, value);
+                break;
+            case EaseType.easeInOutSine:
+                result = easeInOutSine(start, end, value);
+                break;
+            case EaseType.easeInExpo:
+                result = easeInExpo(start, end, value);
+                break;
+            case EaseType.easeOutExpo:
+                result = easeOutExpo(start, end, value);
+                break;
+            case EaseType.easeInOutExpo:
+                result = easeInOutExpo(start, end, value);
+                break;
+            case EaseType.easeInCirc:
+                result = easeInCirc(start, end, value);
+                break;
+            case EaseType.easeOutCirc:
+                result = easeOutCirc(start, end, value);
+                break;
+            case EaseType.easeInOutCirc:
+                result = easeInOutCirc(start, end, value);
+                break;
+            case EaseType.linear:
+                result = linear(start, end, value);
+                break;
+            case EaseType.spring:
+                result = spring(start, end, value);
+                break;
+            /* GFX47 MOD START */
+            /*case EaseType.bounce:
+                result = bounce(start, end, value);
+                break;*/
+            case EaseType.easeInBounce:
+                result = easeInBounce(start, end, value);
+                break;
+            case EaseType.easeOutBounce:
+                result = easeOutBounce(start, end, value);
+                break;
+            case EaseType.easeInOutBounce:
+                result = easeInOutBounce(start, end, value);
+                break;
+            /* GFX47 MOD END */
+            case EaseType.easeInBack:
+                result = easeInBack(start, end, value);
+                break;
+            case EaseType.easeOutBack:
+                result = easeOutBack(start, end, value);
+                break;
+            case EaseType.easeInOutBack:
+                result = easeInOutBack(start, end, value);
+                break;
+            /* GFX47 MOD START */
+            /*case EaseType.elastic:
+                result = elastic(start, end, value);
+                break;*/
+            case EaseType.easeInElastic:
+                result = easeInElastic(start, end, value);
+                break;
+            case EaseType.easeOutElastic:
+                result = easeOutElastic(start, end, value);
+                break;
+            case EaseType.easeInOutElastic:
+                result = easeInOutElastic(start, end, value);
+                break;
+        }
+        return result;
+    }
+
+    
+
+    private static float linear(float start, float end, float value)
+    {
+        return Mathf.Lerp(start, end, value);
+    }
+
+    private static float clerp(float start, float end, float value)
+    {
+        float min = 0.0f;
+        float max = 360.0f;
+        float half = Mathf.Abs((max - min) * 0.5f);
+        float retval = 0.0f;
+        float diff = 0.0f;
+        if ((end - start) < -half)
+        {
+            diff = ((max - start) + end) * value;
+            retval = start + diff;
+        }
+        else if ((end - start) > half)
+        {
+            diff = -((max - end) + start) * value;
+            retval = start + diff;
+        }
+        else retval = start + (end - start) * value;
+        return retval;
+    }
+
+    private static float spring(float start, float end, float value)
+    {
+        value = Mathf.Clamp01(value);
+        value = (Mathf.Sin(value * Mathf.PI * (0.2f + 2.5f * value * value * value)) * Mathf.Pow(1f - value, 2.2f) + value) * (1f + (1.2f * (1f - value)));
+        return start + (end - start) * value;
+    }
+
+    private static float easeInQuad(float start, float end, float value)
+    {
+        end -= start;
+        return end * value * value + start;
+    }
+
+    private static float easeOutQuad(float start, float end, float value)
+    {
+        end -= start;
+        return -end * value * (value - 2) + start;
+    }
+
+    private static float easeInOutQuad(float start, float end, float value)
+    {
+        value /= .5f;
+        end -= start;
+        if (value < 1) return end * 0.5f * value * value + start;
+        value--;
+        return -end * 0.5f * (value * (value - 2) - 1) + start;
+    }
+
+    private static float easeInCubic(float start, float end, float value)
+    {
+        end -= start;
+        return end * value * value * value + start;
+    }
+
+    private static float easeOutCubic(float start, float end, float value)
+    {
+        value--;
+        end -= start;
+        return end * (value * value * value + 1) + start;
+    }
+
+    private static float easeInOutCubic(float start, float end, float value)
+    {
+        value /= .5f;
+        end -= start;
+        if (value < 1) return end * 0.5f * value * value * value + start;
+        value -= 2;
+        return end * 0.5f * (value * value * value + 2) + start;
+    }
+
+    private static float easeInQuart(float start, float end, float value)
+    {
+        end -= start;
+        return end * value * value * value * value + start;
+    }
+
+    private static float easeOutQuart(float start, float end, float value)
+    {
+        value--;
+        end -= start;
+        return -end * (value * value * value * value - 1) + start;
+    }
+
+    private static float easeInOutQuart(float start, float end, float value)
+    {
+        value /= .5f;
+        end -= start;
+        if (value < 1) return end * 0.5f * value * value * value * value + start;
+        value -= 2;
+        return -end * 0.5f * (value * value * value * value - 2) + start;
+    }
+
+    private static float easeInQuint(float start, float end, float value)
+    {
+        end -= start;
+        return end * value * value * value * value * value + start;
+    }
+
+    private static float easeOutQuint(float start, float end, float value)
+    {
+        value--;
+        end -= start;
+        return end * (value * value * value * value * value + 1) + start;
+    }
+
+    private static float easeInOutQuint(float start, float end, float value)
+    {
+        value /= .5f;
+        end -= start;
+        if (value < 1) return end * 0.5f * value * value * value * value * value + start;
+        value -= 2;
+        return end * 0.5f * (value * value * value * value * value + 2) + start;
+    }
+
+    private static float easeInSine(float start, float end, float value)
+    {
+        end -= start;
+        return -end * Mathf.Cos(value * (Mathf.PI * 0.5f)) + end + start;
+    }
+
+    private static float easeOutSine(float start, float end, float value)
+    {
+        end -= start;
+        return end * Mathf.Sin(value * (Mathf.PI * 0.5f)) + start;
+    }
+
+    private static float easeInOutSine(float start, float end, float value)
+    {
+        end -= start;
+        return -end * 0.5f * (Mathf.Cos(Mathf.PI * value) - 1) + start;
+    }
+
+    private static float easeInExpo(float start, float end, float value)
+    {
+        end -= start;
+        return end * Mathf.Pow(2, 10 * (value - 1)) + start;
+    }
+
+    private static float easeOutExpo(float start, float end, float value)
+    {
+        end -= start;
+        return end * (-Mathf.Pow(2, -10 * value) + 1) + start;
+    }
+
+    private static float easeInOutExpo(float start, float end, float value)
+    {
+        value /= .5f;
+        end -= start;
+        if (value < 1) return end * 0.5f * Mathf.Pow(2, 10 * (value - 1)) + start;
+        value--;
+        return end * 0.5f * (-Mathf.Pow(2, -10 * value) + 2) + start;
+    }
+
+    private static float easeInCirc(float start, float end, float value)
+    {
+        end -= start;
+        return -end * (Mathf.Sqrt(1 - value * value) - 1) + start;
+    }
+
+    private static float easeOutCirc(float start, float end, float value)
+    {
+        value--;
+        end -= start;
+        return end * Mathf.Sqrt(1 - value * value) + start;
+    }
+
+    private static float easeInOutCirc(float start, float end, float value)
+    {
+        value /= .5f;
+        end -= start;
+        if (value < 1) return -end * 0.5f * (Mathf.Sqrt(1 - value * value) - 1) + start;
+        value -= 2;
+        return end * 0.5f * (Mathf.Sqrt(1 - value * value) + 1) + start;
+    }
+
+    /* GFX47 MOD START */
+    private static float easeInBounce(float start, float end, float value)
+    {
+        end -= start;
+        float d = 1f;
+        return end - easeOutBounce(0, end, d - value) + start;
+    }
+    /* GFX47 MOD END */
+
+    /* GFX47 MOD START */
+    //private static float bounce(float start, float end, float value){
+    private static float easeOutBounce(float start, float end, float value)
+    {
+        value /= 1f;
+        end -= start;
+        if (value < (1 / 2.75f))
+        {
+            return end * (7.5625f * value * value) + start;
+        }
+        else if (value < (2 / 2.75f))
+        {
+            value -= (1.5f / 2.75f);
+            return end * (7.5625f * (value) * value + .75f) + start;
+        }
+        else if (value < (2.5 / 2.75))
+        {
+            value -= (2.25f / 2.75f);
+            return end * (7.5625f * (value) * value + .9375f) + start;
+        }
+        else {
+            value -= (2.625f / 2.75f);
+            return end * (7.5625f * (value) * value + .984375f) + start;
+        }
+    }
+    /* GFX47 MOD END */
+
+    /* GFX47 MOD START */
+    private static float easeInOutBounce(float start, float end, float value)
+    {
+        end -= start;
+        float d = 1f;
+        if (value < d * 0.5f) return easeInBounce(0, end, value * 2) * 0.5f + start;
+        else return easeOutBounce(0, end, value * 2 - d) * 0.5f + end * 0.5f + start;
+    }
+    /* GFX47 MOD END */
+
+    private static float easeInBack(float start, float end, float value)
+    {
+        end -= start;
+        value /= 1;
+        float s = 1.70158f;
+        return end * (value) * value * ((s + 1) * value - s) + start;
+    }
+
+    private static float easeOutBack(float start, float end, float value)
+    {
+        float s = 1.70158f;
+        end -= start;
+        value = (value) - 1;
+        return end * ((value) * value * ((s + 1) * value + s) + 1) + start;
+    }
+
+    private static float easeInOutBack(float start, float end, float value)
+    {
+        float s = 1.70158f;
+        end -= start;
+        value /= .5f;
+        if ((value) < 1)
+        {
+            s *= (1.525f);
+            return end * 0.5f * (value * value * (((s) + 1) * value - s)) + start;
+        }
+        value -= 2;
+        s *= (1.525f);
+        return end * 0.5f * ((value) * value * (((s) + 1) * value + s) + 2) + start;
+    }
+
+    private static float punch(float amplitude, float value)
+    {
+        float s = 9;
+        if (value == 0)
+        {
+            return 0;
+        }
+        else if (value == 1)
+        {
+            return 0;
+        }
+        float period = 1 * 0.3f;
+        s = period / (2 * Mathf.PI) * Mathf.Asin(0);
+        return (amplitude * Mathf.Pow(2, -10 * value) * Mathf.Sin((value * 1 - s) * (2 * Mathf.PI) / period));
+    }
+
+    /* GFX47 MOD START */
+    private static float easeInElastic(float start, float end, float value)
+    {
+        end -= start;
+
+        float d = 1f;
+        float p = d * .3f;
+        float s = 0;
+        float a = 0;
+
+        if (value == 0) return start;
+
+        if ((value /= d) == 1) return start + end;
+
+        if (a == 0f || a < Mathf.Abs(end))
+        {
+            a = end;
+            s = p / 4;
+        }
+        else {
+            s = p / (2 * Mathf.PI) * Mathf.Asin(end / a);
+        }
+
+        return -(a * Mathf.Pow(2, 10 * (value -= 1)) * Mathf.Sin((value * d - s) * (2 * Mathf.PI) / p)) + start;
+    }
+    /* GFX47 MOD END */
+
+    /* GFX47 MOD START */
+    //private static float elastic(float start, float end, float value){
+    private static float easeOutElastic(float start, float end, float value)
+    {
+        /* GFX47 MOD END */
+        //Thank you to rafael.marteleto for fixing this as a port over from Pedro's UnityTween
+        end -= start;
+
+        float d = 1f;
+        float p = d * .3f;
+        float s = 0;
+        float a = 0;
+
+        if (value == 0) return start;
+
+        if ((value /= d) == 1) return start + end;
+
+        if (a == 0f || a < Mathf.Abs(end))
+        {
+            a = end;
+            s = p * 0.25f;
+        }
+        else {
+            s = p / (2 * Mathf.PI) * Mathf.Asin(end / a);
+        }
+
+        return (a * Mathf.Pow(2, -10 * value) * Mathf.Sin((value * d - s) * (2 * Mathf.PI) / p) + end + start);
+    }
+
+    /* GFX47 MOD START */
+    private static float easeInOutElastic(float start, float end, float value)
+    {
+        end -= start;
+
+        float d = 1f;
+        float p = d * .3f;
+        float s = 0;
+        float a = 0;
+
+        if (value == 0) return start;
+
+        if ((value /= d * 0.5f) == 2) return start + end;
+
+        if (a == 0f || a < Mathf.Abs(end))
+        {
+            a = end;
+            s = p / 4;
+        }
+        else {
+            s = p / (2 * Mathf.PI) * Mathf.Asin(end / a);
+        }
+
+        if (value < 1) return -0.5f * (a * Mathf.Pow(2, 10 * (value -= 1)) * Mathf.Sin((value * d - s) * (2 * Mathf.PI) / p)) + start;
+        return a * Mathf.Pow(2, -10 * (value -= 1)) * Mathf.Sin((value * d - s) * (2 * Mathf.PI) / p) * 0.5f + end + start;
+    }
+    /* GFX47 MOD END */
+
+    #endregion
+}
+
---
+++ TestCameralCtrl.cs
@@ -0,0 +1,105 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestCameralCtrl : MonoBehaviour {
+    public MathUtils.EaseType easeType = MathUtils.EaseType.easeInExpo;
+    public GameObject Cam;
+    public float speed;
+
+    public Vector2 pos_min;
+    public Vector2 pos_max;
+
+    public bool rotate;
+    public Vector3 angleX_min;
+    public Vector3 angleX_max;
+    public Vector3 angleZ_min;
+    public Vector3 angleZ_max;
+
+    bool dragging = false;
+    Vector3 startDragPos;
+    Vector3 currDragPos;
+    Vector3 targetMovingPos;
+    Vector3 startMovingPos;
+    Vector3 currMovingPos;
+    float startMovingTime;
+
+    Vector2 movingRatio;
+    Quaternion cameraRotDistant;
+
+    void replace(Vector3 s, ref Vector3 d) {
+        if (s.x != 0) {
+            d.x = s.x;
+        } else if (s.y != 0) {
+            d.y = s.y;
+        } else if (s.z != 0) {
+            d.z = s.z;
+        }
+    }
+    
+    void Start () {
+        movingRatio.x = 100f / ((float)Screen.width);
+        movingRatio.y = 100f / ((float)Screen.height);
+
+        Vector3 pos = Cam.transform.localPosition;
+        startMovingPos = pos;
+        currMovingPos = pos;
+        targetMovingPos = pos;
+        cameraRotDistant = Cam.transform.rotation;
+	}
+		
+	void Update () {
+        currMovingPos.y = 0;
+        updateCamera();
+
+        Vector3 finalPos = currMovingPos;
+        finalPos.x = Mathf.Clamp(finalPos.x, pos_min.x, pos_max.x);
+        finalPos.z = Mathf.Clamp(finalPos.z, pos_min.y, pos_max.y);
+        currMovingPos = finalPos;
+        Cam.transform.localPosition = currMovingPos;
+
+        if (rotate) {
+            float ratioX = (finalPos.x - pos_min.x) / (pos_max.x - pos_min.x);
+            float ratioZ = (finalPos.z - pos_min.y) / (pos_max.y - pos_min.y);
+            Vector3 angleX = Vector3.Lerp(angleX_min, angleX_max, ratioX);
+            Vector3 angleZ = Vector3.Lerp(angleZ_min, angleZ_max, ratioZ);
+            Vector3 angle = Cam.transform.localEulerAngles;
+            replace(angleX, ref angle);
+            replace(angleZ, ref angle);
+            Cam.transform.localEulerAngles = angle;
+        }
+	}
+
+    void updateCamera() {
+        if (Input.GetMouseButtonDown(0)) {
+            dragging = true;
+            startDragPos = Input.mousePosition;
+
+            targetMovingPos = currMovingPos;
+            startMovingPos = targetMovingPos;
+
+            cameraRotDistant = Quaternion.Euler(Cam.transform.rotation.eulerAngles);
+            startMovingTime = Time.time;
+        } else if (Input.GetMouseButton(0) && dragging) {
+            dragging = true;
+            currDragPos = Input.mousePosition;
+        } else {
+            dragging = false;
+        }
+
+        Vector3 delta;
+        if (dragging) {
+            delta = currDragPos - startDragPos;
+            float movingX = delta.x * movingRatio.x * speed;
+            float movingY = delta.y * movingRatio.y * speed;
+            targetMovingPos = startMovingPos + cameraRotDistant * new Vector3(-movingX, 0, -movingY);
+            //targetMovingPos = startMovingPos + new Vector3(movingX, 0, movingY);
+        }
+
+        Vector3 pos = currMovingPos;
+        delta = targetMovingPos - pos;
+        pos.x = MathUtils.ease(pos.x, targetMovingPos.x + delta.x * 1.2f, Time.deltaTime, easeType);
+        pos.z = MathUtils.ease(pos.z, targetMovingPos.z + delta.z * 1.2f, Time.deltaTime, easeType);
+        currMovingPos = pos;
+    }
+}
